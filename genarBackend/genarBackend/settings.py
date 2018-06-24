6755eab43ea6821eeff0f70a7428e4a5da7a8c2c
Ivan <alialmandoz@gmail.com>
2018-06-23 23:12:31 -0300

    seagrego la configuracion necesaria para que el modelo pueda cargar imagenes en la carpeta imagenes.

--- genarBackend/genarBackend/settings.py
+++ genarBackend/genarBackend/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-MEDIA_ROOT = './images/'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'integrantes/images/')
 
 STATIC_ROOT = ''
 

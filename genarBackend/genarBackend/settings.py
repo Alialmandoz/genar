9457ba8dfb7e80737448eb879896afcb35061517
Ivan <alialmandoz@gmail.com>
2018-06-26 20:16:35 -0300

    configure settings.py para manejar archivos staticos.

--- genarBackend/genarBackend/settings.py
+++ genarBackend/genarBackend/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'genarBackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/genarBackend/integrantes/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,13 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'integrantes/images/')
 
-STATIC_ROOT = ''
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'integrantes/static/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+

efaefee0df0c26f92e5f1bd9723cf089f48a18e3
Ivan <alialmandoz@gmail.com>
2018-06-26 20:21:08 -0300

    agregue la url a la base del proyecto para que tome la plantilla index como entrypoit.

--- genarBackend/integrantes/urls.py
+++ genarBackend/integrantes/urls.py
@@ -3,6 +3,7 @@ from integrantes import views
 
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^integrantes/$', views.integrantes_list),
     url(r'^integrantes/(?P<pk>[0-9]+)/$', views.integrante_detail),
 ]
